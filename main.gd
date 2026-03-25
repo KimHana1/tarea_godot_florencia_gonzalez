9ee80614fb7dc6e167ffa50dac0f558aa43b8f60
KimHana1 <104699494+KimHana1@users.noreply.github.com>
2026-03-25 20:58:50 -0300

    instanciar hud

--- main.gd
+++ main.gd
@@ -5,17 +5,23 @@ var score
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	get_tree().call_group("mobs", "queue_free")
+
+
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
+
 func _on_mob_timer_timeout():
 
 	var mob = mob_scene.instantiate()
@@ -33,5 +39,8 @@ func _on_mob_timer_timeout():
 	mob.linear_velocity = velocity.rotated(direction)
 
 	add_child(mob)
+
 func _ready():
 	new_game()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
